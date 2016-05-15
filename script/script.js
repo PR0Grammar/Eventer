849fc8dd9210061b40f0da3c4de31f0f9da39e50
pr0grammar <npatel3030@gmail.com>
2016-05-15 15:30:26 +0000

    Added random colored boxes. Adjusted height and width of boxes with text

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     idealTemp();
     locationSet();
     currentLocationSet();
+    randomBoxColors();
     resizeBlocks();
     $(window).resize(function() {
         resizeBlocks();
@@ -10,19 +11,37 @@ $(document).ready(function(){
     clickBoxes();
 });
 
-//boxes
+//Boxes
+
+function randomBoxColors(){
+    var colors = ["#7FDBFF","#0074D9","#001f3f","#39CCCC","#3D9970","#2ECC40","#01FF70","#FFDC00","#FF851B","#FF4136","#85144b","#F012BE","#B10DC9","#AAAAAA","#DDDDDD"];
+    $(".box").each(function(){
+        $(this).css('background', colors[Math.floor(Math.random()*colors.length)]);
+    });
+}
 
 function resizeBlocks(){
-    $('.color1').height($('.color1').width()/2);
-    $('.color2').height($('.color2').width()/2);
-    $('.color3').height($('.color3').width()/2);
-    $('.color4').height($('.color4').width()/2);
-    $('.color5').height($('.color1').width()/2);
-    $('.color6').height($('.color1').width()/2);
+    $('.box').height($('.box').width()/3);
+    //do the same thing for the width so it fits smaller.
 }
 
 function clickBoxes(){
     $("#box1").click(function(){
+        alert ("clickerino");
+    });
+    $("#box2").click(function(){
+        alert ("clickerino");
+    }); 
+    $("#box3").click(function(){
+        alert ("clickerino");
+    }); 
+    $("#box4").click(function(){
+        alert ("clickerino");
+    }); 
+    $("#box5").click(function(){
+        alert ("clickerino");
+    }); 
+    $("#box6").click(function(){
         alert ("clickerino");
     }); 
 }
@@ -75,13 +94,29 @@ function tempDisplay(temp) {
 function locationSet () {
     $('.btn-setLoc').click(function() {
         var zipcode= $("#zipcodeForm").val();
-        var url="https://maps.googleapis.com/maps/api/geocode/json?address="+zipcode+"&key=AIzaSyD_-eLfqpSYqLRJBiDKv2ve2GP7HO-8hH4";
-        $.getJSON(url,function(data){
-            var city=data.results[0].address_components[1].long_name
-            var zip= data.results[0].address_components[0].long_name
-            $('#locationDisplay').text("Your Location:"+city+","+zip)
-            //USE WEATHER FUNCTION HERE WITH ZIP CODE
-    })
+        if(zipcode==""){
+            alert("Enter a Zipcode")
+            locationSet().stop()
+
+        }
+        else if(isNaN(zipcode)){
+            alert("Enter a Valid Zipcode")
+            locationSet().stop()
+        }
+        else if(zipcode.length>5 || zipcode.length<5){
+            alert("Enter a Valid Zipcode")
+            locationSet().stop()
+        }
+        else{
+            var url="https://maps.googleapis.com/maps/api/geocode/json?address="+zipcode+"&key=AIzaSyD_-eLfqpSYqLRJBiDKv2ve2GP7HO-8hH4";
+            $.getJSON(url,function(data){
+                var city=data.results[0].address_components[1].long_name
+                var zip= data.results[0].address_components[0].long_name
+                var country=data.results[0].address_components[5].short_name
+                $('#locationDisplay').text("Your Location:"+city+","+zip)
+                //USE WEATHER FUNCTION HERE WITH ZIP CODE
+            })
+        }
     })
 }
 
@@ -107,18 +142,25 @@ function getZip(latitude,longitude){
     $.getJSON(url,function(data){
             var city=data.results[0].address_components[2].long_name
             var zip= data.results[0].address_components[7].long_name
+            var country=data.results[0].address_components[6].short_name
             $('#locationDisplay').text("Your Location:"+city+","+zip)
-            //USE WEATHER FUNCTION HERE WITH ZIP CODE
+            getForecast(zip,country);
     })
     
 }
 
 //OpenWeather
 
-function getForecast(zipcode){
-    var url="http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}"
+function getForecast(zipcode,country){
+    var api_key="623c7e489e6b97e211bd1d20ccea234d";
+    var url="http://api.openweathermap.org/data/2.5/weather?zip="+zipcode+","+country+"&appid="+api_key;
+    $.getJSON(url, function(data) {
+        console.log(data);
+    })
 
 }
+
 //api key for openweathermap = 623c7e489e6b97e211bd1d20ccea234d
+//Wunder=f9dfa1e87efd1b7d
 
 
