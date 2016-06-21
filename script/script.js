73af8c502c0cd49788aacdbf3ae27246602fbc6b
pr0grammar <npatel3030@gmail.com>
2016-06-21 17:46:49 +0000

    callBack function test

--- script/script.js
+++ script/script.js
@@ -138,7 +138,20 @@ function getForecast(latitude, longitude){
     var api_key="623c7e489e6b97e211bd1d20ccea234d";
     var url="http://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&units=imperial&appid="+api_key;
     $.getJSON(url, function(data) {
-                $.each(data,function(key,list){
+                callBack();
+    });
+}
+
+var idealOutdoorTemp;
+function outputUpdate(temp) {
+	document.querySelector('#temp').value = temp;
+	idealOutdoorTemp = temp;
+}
+
+
+function callBack (data) {
+    console.log(data);
+        $.each(data.list,function(i,value){
             for(var i=0; i<list.length; i++){
                 if(data.list[i].dt_txt.includes("12:00:00")){
                     var temperature=data.list[i].main.temp;
@@ -157,39 +170,7 @@ function getForecast(latitude, longitude){
                 }
             }
         });
-
-    });
 }
-
-var idealOutdoorTemp;
-function outputUpdate(temp) {
-	document.querySelector('#temp').value = temp;
-	idealOutdoorTemp = temp;
-}
-
-
-// function callBack (data) {
-//     console.log(data);
-//         // $.each(data,function(key,list){
-//         //     for(var i=0; i<list.length; i++){
-//         //         if(data.list[i].dt_txt.includes("12:00:00")){
-//         //             var temperature=data.list[i].main.temp;
-//         //             var weather_condition=data.list[i].weather[0].main;
-//         //             console.log(temperature);
-//         //             console.log(weather_condition);
-//         //             console.log(idealOutdoorTemp);
-//         //             movieCondition(temperature,weather_condition,idealOutdoorTemp);
-//         //             campCondition(temperature, weather_condition,idealOutdoorTemp);
-//         //             parkCondition(temperature, weather_condition,idealOutdoorTemp);
-//         //             eatingCondition(temperature,weather_condition,idealOutdoorTemp);
-//         //             beachCondition(temperature,weather_condition,idealOutdoorTemp);
-//         //             volunteerCondition(temperature,weather_condition,idealOutdoorTemp);
-//         //             // var formatted = $.datepicker.formatDate("M d, yy", new Date(data.list[i].dt_txt));
-//         //             // console.log(formatted);
-//         //         }
-//         //     }
-//         // });
-// }
 
 
 
